d259c4569d67e7363cf0806a29390b8ff4b338b7
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-05-06 10:37:33 +0700

    Add files via upload
    initial commit

---
+++ lib/methods/common_methods.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:drivers_app/global/global_var.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_geofire/flutter_geofire.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/direction_details.dart';
+
+class CommonMethods
+{
+  checkConnectivity(BuildContext context) async
+  {
+    var connectionResult = await Connectivity().checkConnectivity();
+
+    if(connectionResult != ConnectivityResult.mobile && connectionResult != ConnectivityResult.wifi)
+    {
+      if(!context.mounted) return;
+      displaySnackBar("your Internet is not Available. Check your connection. Try Again.", context);
+    }
+  }
+
+  displaySnackBar(String messageText, BuildContext context)
+  {
+    var snackBar = SnackBar(content: Text(messageText));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  turnOffLocationUpdatesForHomePage()
+  {
+    positionStreamHomePage!.pause();
+
+    Geofire.removeLocation(FirebaseAuth.instance.currentUser!.uid);
+  }
+
+  turnOnLocationUpdatesForHomePage()
+  {
+    positionStreamHomePage!.resume();
+
+    Geofire.setLocation(
+      FirebaseAuth.instance.currentUser!.uid,
+      driverCurrentPosition!.latitude,
+      driverCurrentPosition!.longitude,
+    );
+  }
+
+  static sendRequestToAPI(String apiUrl) async
+  {
+    http.Response responseFromAPI = await http.get(Uri.parse(apiUrl));
+
+    try
+    {
+      if(responseFromAPI.statusCode == 200)
+      {
+        String dataFromApi = responseFromAPI.body;
+        var dataDecoded = jsonDecode(dataFromApi);
+        return dataDecoded;
+      }
+      else
+      {
+        return "error";
+      }
+    }
+    catch(errorMsg)
+    {
+      return "error";
+    }
+  }
+
+  static Future<DirectionDetails?> getDirectionDetailsFromAPI(LatLng source, LatLng destination) async {
+    String urlDirectionsAPI = "https://maps.googleapis.com/maps/api/directions/json?destination=${destination.latitude},${destination.longitude}&origin=${source.latitude},${source.longitude}&mode=driving&key=$googleMapKey";
+
+    var responseFromDirectionsAPI = await sendRequestToAPI(urlDirectionsAPI);
+
+    if(responseFromDirectionsAPI == "error" || responseFromDirectionsAPI["routes"].isEmpty || responseFromDirectionsAPI["routes"][0]["legs"].isEmpty) {
+      return null;
+    }
+
+    DirectionDetails detailsModel = DirectionDetails();
+
+    var firstRoute = responseFromDirectionsAPI["routes"][0];
+    var firstLeg = firstRoute["legs"][0];
+
+    detailsModel.distanceTextString = firstLeg["distance"]["text"];
+    detailsModel.distanceValueDigits = firstLeg["distance"]["value"];
+
+    detailsModel.durationTextString = firstLeg["duration"]["text"];
+    detailsModel.durationValueDigits = firstLeg["duration"]["value"];
+
+    detailsModel.encodedPoints = firstRoute["overview_polyline"]["points"];
+
+    return detailsModel;
+  }
+
+
+  calculateFareAmount(DirectionDetails directionDetails)
+  {
+    double distancePerKmAmount = 0.4;
+    double durationPerMinuteAmount = 0.3;
+    double baseFareAmount = 2;
+
+    double totalDistanceTravelFareAmount = (directionDetails.distanceValueDigits! / 1000) * distancePerKmAmount;
+    double totalDurationSpendFareAmount = (directionDetails.durationValueDigits! / 60) * durationPerMinuteAmount;
+
+    double overAllTotalFareAmount = baseFareAmount + totalDistanceTravelFareAmount + totalDurationSpendFareAmount;
+
+    return overAllTotalFareAmount.toStringAsFixed(1);
+  }
+}
